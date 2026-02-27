4a570e1cdc07763f9148ba67a48650ec11d09c24
thongdzai <buivanthong10052004@gmail.com>
2026-02-27 16:29:03 +0700

    update user

---
+++ RecruitmentPlatform/RecruitmentInterviewManagementSystem/Applications/Features/Interface/IMinIOCV.cs
@@ -0,0 +1,10 @@
+﻿namespace RecruitmentInterviewManagementSystem.Applications.Features.Interface
+{
+    public interface IMinIOCV
+    {
+        Task<string> UploadAsync(IFormFile file);
+        Task DeleteAsync(string objectName);
+
+        Task<string> GetUrlImage(string bucket, string imageName);
+    }
+}
