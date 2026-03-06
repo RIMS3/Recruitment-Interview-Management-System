fc4baa4cb5ca5202cacb30b68ba2ab352e4d88c2
thongdzai <buivanthong10052004@gmail.com>
2026-03-06 13:27:47 +0700

    update: update dashboash

---
+++ RecruitmentPlatform/RecruitmentInterviewManagementSystem/Applications/Features/Auth/DTO/DashboardDataDto.cs
@@ -0,0 +1,24 @@
+﻿namespace RecruitmentInterviewManagementSystem.Applications.Features.Auth.DTO
+{
+    public class DashboardDataDto
+    {
+        // KPI Cards
+        public int TotalCandidates { get; set; }
+        public int TotalEmployers { get; set; }
+        public int OpenJobs { get; set; }
+        public int ClosedJobs { get; set; }
+        public int TotalCVs { get; set; }
+
+        public int AppliesToday { get; set; }
+        public int AppliesThisWeek { get; set; }
+        public int AppliesThisMonth { get; set; }
+
+        // Dữ liệu cho Biểu đồ đường (Lượt ứng tuyển 7 ngày qua)
+        public List<ChartData> ApplicationTrends { get; set; }
+    }
+
+    public class ChartData
+    {
+        public string Date { get; set; }
+        public int Applies { get; set; }
+    }}
